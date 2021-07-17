00af677b5f8fdd054de08698a11b4bd7726441de
norbert10 <norbertguda94@gmail.com>
2021-07-17 12:27:20 +0300

    backend

--- server/client/src/components/Login.js
+++ server/client/src/components/Login.js
@@ -1,18 +1,25 @@
 import React, { useState, Component } from 'react'
+import Axios from 'axios'
 import '../styles/Login.css'
 
 export class Login extends Component {
 
     constructor(props) {
         super(props)
-        
+
         this.state = {
             showLogin: true,
             btnText: 'Register',
-        
+            usernameReg: '',
+            passwordReg: '',
+            message: ''
+
+
         }
         this.toggleForm = this.toggleForm.bind(this);
-        
+        this.changeHandler = this.changeHandler.bind(this)
+        this.register = this.register.bind(this)
+
     }
 
     toggleForm() {
@@ -28,6 +35,36 @@ export class Login extends Component {
                 document.getElementById('loginform').style.display = "none"
             })
         }
+    }
+
+    // register(){
+    //     Axios.post('http://localhost:3001/Login', {
+    //         username: username,
+    //         password: password
+    //     }).then((response)=>{
+    //         console.log(response)
+    //     });
+    // }
+    changeHandler(e) {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+    //to post to the databse
+    register(e) {
+        e.preventDefault();
+        if (this.state.usernameReg === '' || this.state.passwordReg === '') {
+            alert("cannot be empty")
+        } else {
+            Axios.post('/userlogin', {
+                username: this.usernameReg,
+                password: this.passwordReg
+            }).then((response) => {
+                console.log(response)
+            }).catch((err) => {
+                alert(err.message);
+
+            })
+        }
+
     }
 
     render() {
@@ -61,26 +98,26 @@ export class Login extends Component {
                     <button id="loginBtn" class="active toggle-btn" onClick={this.toggleForm}>{this.state.btnText}</button>
                 </div>
                 <div className="form-group">
-                    <form action='' id='loginform'>
+                    <form id='loginform'>
                         <label for="username">USERNAME/EMAIL</label>
-                        <input type="text" id="username" />
+                        <input type="text" name="usernameReg" value={this.state.usernameReg} onChange={this.changeHandler} id="username" />
                         <label for="password">PASSWORD</label>
-                        <input type="password" id="password"  required/>
-                        <input type="submit" value="login" className="submit" />
+                        <input type="password" name="passwordReg" value={this.state.passwordReg} onChange={this.changeHandler} id="password" required />
+                        <input type="button" value="login" className="submit" onClick={this.register} />
                     </form>
-                    <form action="" id="registerform">
+                    <form id="registerform">
                         <label for="fname">FIRST NAME</label>
-                        <input type="text" id="fname" required/>
+                        <input type="text" id="fname" required />
                         <label for="lname" >LAST NAME</label>
-                        <input type="text" id="lname" required/>
+                        <input type="text" id="lname" required />
                         <label for="password" >PASSWORD</label>
-                        <input type="password" id="pass" required/>
+                        <input type="password" id="pass" required />
                         <label for="cpassword">CONFIRM PASSWORD</label>
-                        <input type="password" id="pass" required/>
+                        <input type="password" id="pass" required />
                         <label for="phone" >PHONE NUMBER</label>
-                        <input type="tel" id="phone" required/>
+                        <input type="tel" id="phone" required />
                         <label for="email">EMAIL ADDRESS</label>
-                        <input type="email" id="email" required/>
+                        <input type="email" id="email" required />
                         <input type="submit" value="register" className="submit" />
                     </form>
 
--- server/index.js
+++ server/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+//to post every object we send from the backend
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json())
+
+// app.use(cors());
 
 //creating a connection
 const db = mysql.createPool({
@@ -10,19 +19,6 @@ const db = mysql.createPool({
     password: '',
     database: 'auction'
 });
-
-function Auth(req, res, next) {
-    let v = false;
-    if (!v) {
-        return  res.redirect('/login');
-    } else {
-        return next()
-    }
-}
-
-app.use(`/`,express.static(path.join(__dirname + '/client/build')));
-
-//To check if the database is connected
 db.getConnection((err) => {
     if (err) {
         console.log(err)
@@ -31,29 +27,71 @@ db.getConnection((err) => {
     }
 })
 
-app.get('/login', (req, res) => {
-    res.send(
-        `<!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-        </head>
-        <body>
-            <H1>Login</H1>
-        </body>
-        </html>`
-    )
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/Login.js')
+});
+
+// app.get("/", (req,res)=>{
+//     const sqlInsert = 'INSERT INTO login (username, password) VALUES ("Evans", "Odhiambo");'
+//     db.query(sqlInsert, (err, result)=>{
+//         res.send("Hello world");  
+//     })
+
+    	                            
+// })
+
+//query to insert new user to the database
+app.post("/userlogin",(req,res)=>{
+    console.log(req.body)
+    const username = req.body.username
+    const password = req.body.password
+    db.query('INSERT INTO login (username, password) VALUES (?,?)', [username, password], (err, result)=>{
+        if(err){
+            console.log(err)
+        }
+        console.log(result);
+        res.status(200).end();
+    })
 })
 
-//for any other request they should br redirected to index.html
-app.get('*',(req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+app.listen(3001, () => {
+    console.log('Server running on port 3001')
 })
 
-app.listen(5000, () => {
-    console.log('Server running on port 5000')
-})
+
+// function Auth(req, res, next) {
+//     let v = false;
+//     if (!v) {
+//         return  res.redirect('/login');
+//     } else {
+//         return next()
+//     }
+// }
+
+// app.use(`/`,express.static(path.join(__dirname + '/client/build')));
+
+// //To check if the database is connected
+
+// app.get('/login', (req, res) => {
+//     res.send(
+//         `<!DOCTYPE html>
+//         <html lang="en">
+//         <head>
+//             <meta charset="UTF-8">
+//             <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//             <title>Document</title>
+//         </head>
+//         <body>
+//             <H1>Login</H1>
+//         </body>
+//         </html>`
+//     )
+// })
+
+// //for any other request they should br redirected to index.html
+// app.get('*',(req, res) => {
+//     res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+// })
+
 
